ebf54fed20e324ea62107172806a19770b6b4b96
Oleh Holovnykh <119495104+oleh-holovnykh@users.noreply.github.com>
2025-05-31 21:45:44 +0300

    Update ScheduleHTML.tsx

--- src/components/ScheduleHTML.tsx
+++ src/components/ScheduleHTML.tsx
@@ -37,7 +37,7 @@ export const ScheduleHTML: React.FC = () => {
       <div className="mt-6">
         <b>🗓️ 01-08.08 Інтенсив Awareness</b>
           <div className='text-gray-500 italic'>
-            <p> <span className='underline'>Вартість участі при передплаті (€50)</span>:</p>
+            <p> <span className='underline'>Вартість участі при передплаті (€100)</span>:</p>
             <p className='ml-4'>до 1 червня — €250</p>
             <p className='ml-4'>до 1 серпня — €300</p>
             <p><span className='underline'>Проживання (за 8 днів для однієї людини)</span>: 5600 (готель 2-х) / 4410 (готель 3-х) / 4200(новий корпус 2-х) / 3850(будинок рибалки 2-х, 4-х) / 3150(хостел)</p>
